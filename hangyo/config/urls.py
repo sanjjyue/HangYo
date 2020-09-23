36660bc23144b0150116cd039a4dbc3bf3aaaf70
songtaeh <sjr19660@naver.com>
2020-09-23 12:59:02 +0900

    [mdf]main.html수정 및 css폴더위치변경

--- hangyo/config/urls.py
+++ hangyo/config/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import Mainpage
+from main.views import mainpage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',Mainpage),
+    path('',mainpage),
 ]
 #test
