991add81bd86e480ab6e8880d3743d13922f8950
mattew8 <mattew4483@likelion.org>
2020-10-04 17:29:41 +0900

    [add] 가맹점 정보 지도 마커로 표시

--- hangyo/main/views.py
+++ hangyo/main/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Stores
 
 # Create your views here.
 def mainpage(request):
-    return render(request,'home.html')
+    all_info = Stores.objects.all()
+    context = {'take_all_info' : all_info}
+    return render(request,'home.html', context)
+
+def storeinfo(request):
+    return render(request, 'home.html', context)
