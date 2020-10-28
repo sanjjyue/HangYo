4d288cedf1d46deb973393af19f19647a2efd5da
xxhyowon <64009222+xxhyowon@users.noreply.github.com>
2020-10-28 22:55:32 +0900

    Revert "[pr] css수정"

--- hangyo/main/forms.py
+++ hangyo/main/forms.py
@@ -4,10 +4,4 @@ from .models import Stores, Report
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ('stores', 'type', 'content')
-    
-    def __init__(self, *args, **kwargs):
-            super().__init__(*args, **kwargs)
-            self.fields['content'].widget.attrs.update({
-                'placeholder' : '제보내용을 입력해 주세요',
-            })
+        fields = "__all__"
