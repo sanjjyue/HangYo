7c9ec0c7e0169b8f06bf84f3ec2cb79891dbbae7
mattew8 <mattew4483@likelion.org>
2020-11-10 23:56:34 +0900

    [mdf] Store모델에서 store_type제거

--- hangyo/main/models.py
+++ hangyo/main/models.py
@@ -6,7 +6,6 @@ class Stores(models.Model):
     store_name = models.CharField(max_length=100)
     phone_num = models.CharField(max_length=100)
     location = models.CharField(max_length=100)
-    store_type = models.CharField(max_length=100)
     longitude = models.FloatField(default=0)
     # 경도
     Latitude = models.FloatField(default=0)
