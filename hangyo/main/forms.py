0cac128e17bedd2c15d3b6c5efc8aee7bcbfa75a
mattew8 <mattew4483@likelion.org>
2020-11-13 15:36:35 +0900

    [mdf]제보기능 수정

--- hangyo/main/forms.py
+++ hangyo/main/forms.py
@@ -4,10 +4,13 @@ from .models import Stores, Report
 class ReportForm(forms.ModelForm):
     class Meta:
         model = Report
-        fields = ('type', 'content')
+        fields = ('type', 'content','name')
 
     def __init__(self, *args, **kwargs):
             super().__init__(*args, **kwargs)
             self.fields['content'].widget.attrs.update({
                 'placeholder' : '제보내용을 입력해 주세요',
+            })
+            self.fields['name'].widget.attrs.update({
+                'placeholder' : '제보가게를 입력해 주세요',
             })
